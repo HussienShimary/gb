e075e91917cd47cc0e96b8de97939ff4686f06bf
HussienShimary <69988927+HussienShimary@users.noreply.github.com>
2020-08-21 03:16:53 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NzQ2MTQ1NDY0NDY1ODgzMTM2.Xz8EHQ.Z9tNemQ9caH38t-7p4XOF5_twfk', /// توكن البوت
+    YT_API_KEY: 'AIzaSyAvQO4T9kuzMlx8VY7b8YMkpD2ou7BEcpE ', /// مش مهم
+    prefix: '?r', // هنا برفكس او مقدمة البوت
+    devs: ['537699689017901115'] //// ايدي حسابك
+  }
